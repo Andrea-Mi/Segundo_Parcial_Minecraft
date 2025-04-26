1132d9f518c02b5d4ef375446dc501b5c19318f3
Tania Andrea <tmirandar@miumg.edu.gt>
2025-04-25 22:30:58 -0600

    CUESTIONARIO

--- MinecraftSP/MinecraftSP/obj/Release/net9.0-windows/MinecraftSP.AssemblyInfo.cs
+++ MinecraftSP/MinecraftSP/obj/Release/net9.0-windows/MinecraftSP.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinecraftSP")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf65e3808a9ab9826c1813ff4002aeffcdc6875a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinecraftSP")]
